b599c7ac775d960966c6eb1afd461dc67b20bb88
Sebastian Grottel <sebastian.grottel@microsoft.com>
2023-09-28 14:39:44 +0200

    Added EtlDump to see the parsed result of `Msft.Diag.Tracing.TraceEvent`

---
+++ EtlDump/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Diagnostics.Tracing;
+
+namespace EtlDump
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (var source = new ETWTraceEventSource(@"etl-demo.etl"))
+            {
+                // setup the callbacks
+                source.Dynamic.All += (TraceEvent ev) => {
+                    if (ev.ProviderName != "EtlProviderApp") return;
+
+                    Console.Write($"{ev.EventName} -- (");
+
+                    foreach (string name in ev.PayloadNames)
+                    {
+                        Console.Write($" '{name}': '{ev.PayloadByName(name)}' ({ev.PayloadByName(name).GetType()})");
+                    }
+
+                    int t = (int)ev.Task;
+                    Console.WriteLine($" ) -- Task: '{t}'");
+                };
+
+                // iterate over the file, calling the callbacks.  
+                source.Process();
+            }
+        }
+
+    }
+}
